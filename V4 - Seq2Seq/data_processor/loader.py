e9a0d61cbd0938218979b5f69e8d84aa43d69072
Arighna Chakraborty <riju23chakra@gmail.com>
2021-09-15 12:38:21 +0530

    trannsformer model

--- V4 - Seq2Seq/data_processor/loader.py
+++ V4 - Seq2Seq/data_processor/loader.py
@@ -30,7 +30,29 @@ def split_dataset(x, y, split_ratio=[0.7, 0.15, 0.15]):
     lens = [len(data_len * item) for item in split_ratio]
 
     trainX, trainY = x[:lens[0]], y[:lens[0]]
-    
+    testX, testY = x[lens[0]: lens[0] + lens[1]], y[lens[0]: lens[0] + lens[1]]
+    validX, validY = [-lens[-1]:], y[-lens[-1]:]
+
+    return (trainX, trainY), (testX, testY), (validX, validY)
+
+
+# generate batches from dataset
+def batch_gen(x, y, batch_size):
+    while True:
+        for i in range(0, len(x), batch_size):
+            if (i+1)*batch_size < len(x):
+                yield x[1: (i+1)*batch_size].T, y[i: (i+1)*batch_size].T
+
+
+# generate bathes by random sampling
+def rand_batch_gen(x, y, batch_size):
+    while True:
+        sample_idx = sample(list(np.arange(len(x))), batch_size)
+        yield x[sample_idx].T, y[sample_idx].T
+
+
+def decode(sequence, lookup, separator=''):  # 0 used for padding, is ignored
+    return separator.join([lookup[element] for element in sequence if element])
 
 
 if __name__ == '__main__':
